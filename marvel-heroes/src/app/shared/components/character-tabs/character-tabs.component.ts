9525c9360ad3fcbf7c206b8d89a01779f49dad16
hugo <hugo.estevez@netcentric.biz>
2025-05-17 11:55:44 +0200

    Create basic component structure

---
+++ marvel-heroes/src/app/shared/components/character-tabs/character-tabs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-tabs',
+  imports: [],
+  templateUrl: './character-tabs.component.html',
+  styleUrl: './character-tabs.component.scss'
+})
+export class CharacterTabsComponent {
+
+}
